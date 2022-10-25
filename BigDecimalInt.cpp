e44af134e92a1c45532c6b649f9047b50b2aaed5
hadeer0161 <hadeer0161@gmail.com>
2022-10-25 20:03:37 +0200

    secondcommit

--- BigDecimalInt.cpp
+++ BigDecimalInt.cpp
@@ -26,6 +26,240 @@ BigDecimalInt::BigDecimalInt() {}
 //Eng Hadeer work
 
 
+// function that calculates the difference of two big decimal numbers
+string BigDecimalInt::difference(string b1 , string b2) {
+
+    string result;
+
+    int reminder = 0, int_result,n_itr = b1.length()-1 ,diff_len = 0;
+
+//    check if the length of the first string is larger than the second and make them equal by filling with zeros on the left
+    if(b1.length()>b2.length()){
+        n_itr = b1.length()-1;
+        diff_len = b1.length()-b2.length();
+        b2.insert(0,diff_len,'0');
+    }
+
+//    check if the length of the second string is larger than the first and make them equal by filling with zeros on the left
+    else if(b2.length()>b1.length()){
+        n_itr = b2.length()-1;
+        diff_len = b2.length()-b1.length();
+        b1.insert(0,diff_len,'0');
+    }
+
+// checking if the first number is greater the second
+    if(b1>b2){
+
+//        subtracting algorithm
+        for (int i = n_itr ; i>=0 ; i--){
+
+            int_result = char_int_map[b1[i]]-char_int_map[b2[i]]-reminder;
+            if( int_result < 0 ){
+                int_result+=10;
+                reminder=1;
+            }
+            else{
+                reminder=0;
+            }
+            result += to_string(int_result)[to_string(int_result).length()-1];
+        }
+    }
+
+
+// checking if the second number is greater the first to add a - sign to the result of subtracting
+    else if(b2>b1){
+
+        //        subtracting algorithm
+        for (int i = n_itr ; i>=0 ; i--){
+
+            int_result = char_int_map[b2[i]]-char_int_map[b1[i]]-reminder;
+            if( int_result < 0 ){
+                int_result+=10;
+                reminder=1;
+            }
+            else{
+                reminder=0;
+            }
+            result += to_string(int_result);
+
+        }
+        result.resize(result.length()+1,'-');
+
+    }
+    else{
+        result='0';
+    }
+
+
+    reverse(result.begin(), result.end());
+
+    return result;
+
+}
+
+
+
+
+// - operator that uses the difference() function to get the difference of two big decimal numbers
+// it covers all the different cases of subtraction numbers with the different signs
+BigDecimalInt BigDecimalInt::operator-(BigDecimalInt anotherDec) {
+
+    BigDecimalInt B1("0000"),B2("00000");
+    string result, b1, b2 ;
+
+
+
+// to cover the case of the difference between two positive numbers written with no + sign
+    if(isdigit(this->sgn) and isdigit(anotherDec.sgn) ){
+        b1 = bigDecimalIint;
+        b2 = anotherDec.bigDecimalIint;
+
+
+        result = difference(b1,b2);
+    }
+
+// to cover the case of the difference between two positive numbers written with  + sign
+    else if(this->sgn=='+' and anotherDec.sgn == '+'){
+        b1 = bigDecimalIint.substr(1,bigDecimalIint.length()-1);
+        b2 = anotherDec.bigDecimalIint.substr(1,anotherDec.bigDecimalIint.length()-1 );
+
+
+        result = difference(b1,b2);
+    }
+
+
+// to cover the case of the difference between one negative number and one positive number written with no + sign
+    else if(this->sign() == '-' and isdigit(anotherDec.sgn) ){
+
+        b1= this->bigDecimalIint.substr(1, this->size());
+        b2= anotherDec.bigDecimalIint;
+        result=sum(b1,b2);
+        result.insert(0,1,'-');
+
+    }
+
+// to cover the case of the difference between one negative number and one positive number written with + sign
+    else if(this->sign() == '-' and anotherDec.sgn=='+' ){
+        b1= this->bigDecimalIint.substr(1, this->size());
+        b2= anotherDec.bigDecimalIint.substr(1,anotherDec.size());
+        result=sum(b1,b2);
+        result.insert(0,1,'-');
+    }
+
+// to cover the case of the difference between one positive number written with no + sign and another positive number written with + sign
+    else if (isdigit(this->sgn) and anotherDec.sgn=='+'){
+        b1= this->bigDecimalIint;
+        b2= anotherDec.bigDecimalIint.substr(1,anotherDec.size());
+        result = difference(b1,b2);
+
+    }
+
+// to cover the case of the difference between one positive number written with sign and another positive number written with no + sign
+    else if (this->sgn =='+' and isdigit(anotherDec.sgn)){
+        b1= this->bigDecimalIint.substr(1, this->size());
+        b2= anotherDec.bigDecimalIint;
+        result = difference(b1,b2);
+
+    }
+
+
+// to cover the case of the difference between two negative numbers written with - sign
+    else if (this->sign()== '-' and anotherDec.sign()=='-'){
+        B1.bigDecimalIint = this->bigDecimalIint.substr(1,this->size());
+        B2.bigDecimalIint = anotherDec.bigDecimalIint.substr(1,anotherDec.size());
+
+        b1 = B1.bigDecimalIint;
+        b2 = B2.bigDecimalIint;
+
+        //        to decide which sign to put according to the comparsion between the two numbers without a sign
+        if(B1>B2){
+            result = difference(b1,b2);
+            result.insert(0,1,'-');
+        }
+        if(B2>B1){
+            result = difference(b1,b2);
+            result=result.substr(1,result.length()-1);
+
+        }
+
+    }
+
+// to cover the case of the difference between one positive number written with no + sign and  one negative number written with - sign
+    else if(isdigit(this->sgn)  and anotherDec.sign()=='-'){
+        b1 = bigDecimalIint;
+        b2 = anotherDec.bigDecimalIint.substr(1,anotherDec.bigDecimalIint.length()-1 );
+
+        result = sum(b1,b2);
+    }
+
+// to cover the case of the difference between one positive number written with  + sign and  one negative number written with - sign
+    else if(this->sgn =='+' and anotherDec.sign()=='-'){
+        b1 = bigDecimalIint.substr(1,anotherDec.bigDecimalIint.length()-1 );
+        b2 = anotherDec.bigDecimalIint.substr(1,anotherDec.bigDecimalIint.length()-1 );
+
+        result = sum(b1,b2);
+    }
+
+    BigDecimalInt difference(result);
+    return difference;
+
+}
+
+
+
+// operator < to compare between two big decimal numbers and  return a true value if the first number was smaller than the second number
+// and return false otherwise
+bool BigDecimalInt::operator<(BigDecimalInt anotherDec) {
+
+//    compare between the two numbers using the sign
+    if ((anotherDec.sign()=='-' and this->sign()=='+')){
+        return  false;
+    }
+
+//    compare between the two numbers using the sign
+    if ((anotherDec.sign()=='+' and this->sign()=='-')){
+        return  true;
+    }
+
+//    compare between the two numbers using the length if the numbers had the same sign
+    else if(bigDecimalIint.length() > anotherDec.bigDecimalIint.length()){
+        return false;
+    }
+
+//   compare between  the two numbers using the value of them if the length and the sign are the same
+    else if(bigDecimalIint.length()==anotherDec.bigDecimalIint.length()){
+
+//        comparing using the string to int conversion for the whole string if the number was formed from less than 10 digits
+        if(bigDecimalIint.length()<10){
+            if(stoi(bigDecimalIint) > stoi(anotherDec.bigDecimalIint) )
+                return true;
+        }
+
+
+// comparing using the string to int conversion for every 9 digits of the string if the number was formed from more than 10 digits
+// until reaching the end of the string
+        int start=0, len= 9,end=bigDecimalIint.length()/9,check=0;
+        string p1 = bigDecimalIint ,p2 = anotherDec.bigDecimalIint;
+        while(true){
+            if(stoi(p1.substr(start, len))>stoi(p2.substr(start,len))){
+                return false;
+            }
+            if( p1.length()-start>9){
+                start+=9;
+            }else{
+                len = p1.length()-start;
+            }
+
+            check+=1;
+            if(check>end){
+                return true;
+            }
+        }
+    }
+    else {
+        return true;
+    }
+}
 
 
 
